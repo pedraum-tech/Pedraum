c5982901869e5e7fc4f26d998ce512700849da8d
Lucas David <lucasdavidoliveira80@gmail.com>
2025-12-24 00:58:54 -0300

    fix: corrigir caminho do estado de armazenamento para testes no Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://localhost:3000',
     // Diz para todos os testes usarem esse estado de login
-    storageState: 'playwright/.auth/user.json',
+    storageState: path.resolve(__dirname, 'playwright/.auth/user.json'),
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
